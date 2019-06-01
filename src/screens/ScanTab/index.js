5aa2f71358f42bc126e9b6bf94e485c41a54d311
mathiasfc <mathias.falci@gmail.com>
2019-06-01 16:10:11 -0300

    :sparkles: reading qr code

--- src/screens/ScanTab/index.js
+++ src/screens/ScanTab/index.js
@@ -1,50 +1,56 @@
 import React, { useEffect } from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import Scanner from '../../components/Scanner';
-import logo from '../../images/logo.svg';
 import * as styles from './style';
-import { login } from '../../store/auth/actions';
-import { createCard, listCards } from '../../store/paymentMethod/actions';
+import {
+  loadingTab,
+  receiveTab,
+  getTabDetails,
+  cancelCameraAccess,
+  activateCamera,
+} from '../../store/tab/actions';
 
-const ScanTab = props => {
+const ScanTab = ({ tab, getTabDetails, cancelCameraAccess, activateCamera }) => {
   useEffect(() => {
     console.log('scan tab didmount');
     // if(!props.voucher.cameraAccess) {
     //   props.show();
     // }
+    //se não tiver premissão para acessar a camera
+    //pedir para digitar
   }, []);
 
-   const onScan = id => {
-     if (id) this.props.getVoucher(id);
-   };
+  const onScan = id => {
+    if (id) alert(id);
+  };
 
   return (
     <div>
-      teste
       <Scanner
         title="Scan Voucher"
         subtitle="Escaneie o voucher para utilizá-lo."
-        // onScan={this.onScan}
-        // hasModalOpen={false}
-        // cameraAccess={voucher.cameraAccess}
-        // cancelCameraAccess={cancelCameraAccess}
-        // activateCamera={false}
+        onScan={onScan}
+        cameraAccess={tab.cameraAccess}
+        cancelCameraAccess={cancelCameraAccess}
+        activateCamera={activateCamera}
       />
     </div>
   );
 };
 
-export default ScanTab;
-// const mapStateToProps = state => ({
-//   auth: state.auth,
-// });
+const mapStateToProps = state => ({
+  tab: state.tab,
+});
 
-// const mapDispatchToProps = dispatch =>
-//   bindActionCreators({ login, createCard, listCards }, dispatch);
+const mapDispatchToProps = dispatch => ({
+  loadingTab: () => dispatch(loadingTab()),
+  receiveTab: () => dispatch(receiveTab()),
+  getTabDetails: id => dispatch(getTabDetails(id)),
+  cancelCameraAccess: () => dispatch(cancelCameraAccess()),
+  activateCamera: () => dispatch(activateCamera()),
+});
 
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(ScanTab);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ScanTab);
